12b1fdaeee4b22d045283ebebfe23359ec6e6cc9
Wojciech Ptak <wojtek.ptak@gmail.com>
2025-05-18 12:42:57 +0200

    subdir

--- regen.sh
+++ regen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pandoc --template=elegant_bootstrap_menu egzamin.md -o egzamin.html --toc --toc-depth=2 --mathjax
+pandoc --template=elegant_bootstrap_menu egzamin.md -o docs/index.html --toc-depth=2 --mathjax
